5655dcd77312c4accfa5d315ee66d6d1d47cb3d1
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2022-02-27 13:27:26 +0200

    add: HOFs with curries

---
+++ src/main/scala/lectures/part3fp/HofsCurries.scala
@@ -0,0 +1,70 @@
+package lectures.part3fp
+
+object HofsCurries extends App {
+
+  val superFunction: (Int, (String, (Int, Boolean)) => Int) => (Int => Int) = null
+  // higher order function (HOF)
+
+  // map, flatMap, filter in MyList
+
+  // function that applies a function n times over a value x
+  // nTimes(f, n, x)
+  // nTimes(f, 3, x) => f(f(f(x))) = nTimes(f,2,f(x))
+  // nTimes(f,n,x) = f(f(...f(x))) = nTimes(f,n-1,f(x))
+
+  def nTimes(f: Int=>Int, n: Int, x: Int): Int = {
+    if(n<=0) x
+    else nTimes(f,n-1,f(x))
+  }
+
+  val plusOne: Int => Int = x => x+1
+  println(plusOne(3))
+  println(nTimes(plusOne,10,1))
+
+  // ntb(f,n) = x => f(f(f...(x)))
+  // increment10 = ntb(plusOne,10) = x => plusOne(plusOne(...(x)))
+//  val y = increment10(1)
+  def nTimesBetter(f: Int => Int, n:Int): (Int => Int) = {
+    if(n <= 0) (x:Int) => x
+    else (x:Int) => nTimesBetter(f,n-1)(f(x))
+  }
+
+  val plus10 = nTimesBetter(plusOne,10)
+  println(plus10(12))
+
+  // curries functions
+  val superAdder:Int => (Int => Int) = (x:Int) => (y:Int) => x+y
+  val add3 = superAdder(3) // y => 3+y
+  println(add3(10))
+  println(superAdder(3)(10))
+
+  // functions with multiple parameter lists
+  def curriedFormatter(c: String)(x: Double): String = c.format(x)
+
+  // need to define the type
+  val standardFormat: (Double => String) = curriedFormatter("%4.2f")
+  val preciseFormat: (Double => String) = curriedFormatter("%10.8f")
+
+  println(standardFormat(Math.PI))
+  println(preciseFormat(Math.PI))
+
+  /*
+    Takeaways
+    Functional programming = working with functions
+    - pass functions as parameters
+    - return functions as results
+
+    => Higher Order Functions (HOFs)
+    => Currying = function with multiple parameter lists
+
+  */
+
+  /*
+    Exercise
+    1. Expand MyList
+      - foreach method A => Unit
+      - sort function ((A,A) => Int)
+      - zipWith (list, (A,A) => B)
+  */
+
+}
